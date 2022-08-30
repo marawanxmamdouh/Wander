8ac9e433b43b13894d5cf21574cf1d6787e4d89b
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-30 23:49:51 +0200

    Add a marker, move the camera, and zoom

--- app/src/main/java/dev/marawanxmamdouh/wander/MapsActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/wander/MapsActivity.kt
@@ -35,10 +35,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val googleEgypt = LatLng(30.020940, 31.424945)
+        map.addMarker(MarkerOptions().position(googleEgypt).title("Google Office Egypt"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(googleEgypt))
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(googleEgypt, 15f))
     }
 
     private fun createMenu() {
