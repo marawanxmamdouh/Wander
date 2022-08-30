e1423d774bcbfec7e061638bc3a947ef70fe2722
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-31 00:09:14 +0200

    Adding markers on POI click

--- app/src/main/java/dev/marawanxmamdouh/wander/MapsActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/wander/MapsActivity.kt
@@ -42,6 +42,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(googleEgypt, 15f))
 
         setMapLongClick(map)
+        setPoiClick(map)
     }
 
     private fun setMapLongClick(map: GoogleMap) {
@@ -58,6 +59,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .title(getString(R.string.dropped_pin))
                     .snippet(snippet)
             )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker?.showInfoWindow()
         }
     }
 
